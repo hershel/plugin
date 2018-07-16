a1776636479f0121c15a9d429bf5d973e935e59f
Corentin Poupry <corentin.poupry@protonmail.com>
2018-07-16 23:40:06 +0200

    test: improve code coverage

--- test/version.test.ts
+++ test/version.test.ts
@@ -2,6 +2,8 @@ import test from 'ava'
 
 import { plugin as p } from '../src'
 
+const meta = Symbol.for('plugin-metadata')
+
 test('should throw if version does not satisfies the plugin requested version', t => {
   function plugin(i, o, n) {
     n()
@@ -23,4 +25,14 @@ test('should throw if version is not a string', t => {
   t.throws(() => p(plugin, { hershel: 1000 }), {
     message: `expect version string, got 'number'`
   })
+})
+
+test('must not appear in metadata', t => {
+  function plugin(i, o, n) {
+    n()
+  }
+
+  p(plugin, { hershel: '>= 1.0.0' })
+
+  t.not(plugin[meta].hershel, '>= 1.0.0')
 })
